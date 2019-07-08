06e547a11d89e56ec8df7c393df28d09f8a05f6e
Tommy Lee <liqihai1987@gmail.com>
2019-07-08 09:43:37 +0800

    新增markdown组件

--- src/pages/login-page/login-page.vue
+++ src/pages/login-page/login-page.vue
@@ -1,53 +1,39 @@
 <template>
+    <v-app>
+        <v-toolbar app>
+            <v-spacer></v-spacer>
+            <v-avatar>
+                <img src="/favicon.icon"/>
+            </v-avatar>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-content app>
+            <v-layout>
+                <v-flex sm12 md8>
 
-    <!--    <div class="login">-->
-    <!--        -->
-    <!--        <Layout>-->
-    <!--            <Card dis-hover class="ivu_login_card">-->
-    <!--                <p slot="title" style="text-align: center">登录</p>-->
-    <!--                <Form ref="formItem" :model="formItem" :rules="ruleValidate" :label-width="60">-->
-    <!--                    <FormItem label="账号" prop="loginName">-->
-    <!--                        <Input type="text" v-model="loginName" placeholder="请输入账号..." class="ivu_login_input"/>-->
-    <!--                    </FormItem>-->
-    <!--                    <FormItem label="密码" prop="passwd">-->
-    <!--                        <Input type="password" v-model="passwd" placeholder="请输入密码..." class="ivu_login_input" @on-enter="handleSubmit($refs['formItem'])" />-->
-    <!--                    </FormItem>-->
-    <!--                    <FormItem :label-width="0">-->
-    <!--                        <Button  type="primary"  @click="handleSubmit($refs['formItem'])" class="ivu_login_btn" long>登录</Button>-->
-    <!--                    </FormItem>-->
-    <!--                </Form>-->
-    <!--            </Card>-->
-    <!--        </Layout>-->
-    <!--    </div>-->
-    <v-container>
-        <v-layout align-center
-                  fill-height
-                  justify-center>
-            <v-scale-transition>
+                </v-flex>
+                <v-flex sm12 md4>
+                    <v-card align-center
+                            justify-center>
+                        <v-layout column slot="default"
+                                  style="padding: 20px 40px;">
+                            <h1 style="text-align: center;padding: 10px">有序</h1>
+                            <v-text-field
+                                    label="手机号/邮箱"
+                                    prepend-icon="assignment_ind"
+                            ></v-text-field>
+                            <v-text-field
+                                    label="密码"
+                                    prepend-icon="lock"
+                            ></v-text-field>
+                            <v-btn color="info" @click="handleSubmit()">登录</v-btn>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-content>
+    </v-app>
 
-                <v-card align-center
-                        justify-center
-                        v-show="showLoginPop"
-                        :height="300" :width="400">
-                    <v-layout column slot="default"
-                              style="padding: 20px 40px;">
-                        <h1 style="text-align: center;padding: 10px">有序</h1>
-                        <v-text-field
-                                label="手机号/邮箱"
-                                prepend-icon="assignment_ind"
-                        ></v-text-field>
-                        <v-text-field
-                                label="密码"
-                                prepend-icon="lock"
-                        ></v-text-field>
-                        <v-btn color="info" @click="handleSubmit()">登录</v-btn>
-                    </v-layout>
-                </v-card>
-            </v-scale-transition>
-
-        </v-layout>
-
-    </v-container>
 </template>
 
 <script>
@@ -57,16 +43,6 @@
     export default {
         name: 'login-page',
         store,
-        data() {
-            return {
-                showLoginPop: false
-            }
-        },
-        mounted() {
-            setTimeout(() => {
-                this.showLoginPop = true
-            }, 1200)
-        },
         computed: {
             loginName: {
                 set(val) {
