60b2ed1531cf47ec54b283adc30c14e82bf9ee84
lqh547424@gmail.com <VgRzgF0o0s1Q9w>
2019-07-09 23:23:39 +0800

    更换组件库

--- src/pages/login-page/login-page.vue
+++ src/pages/login-page/login-page.vue
@@ -8,16 +8,19 @@
             <v-spacer></v-spacer>
         </v-toolbar>
         <v-content app>
-            <v-layout>
-                <v-flex sm12 md8>
-
+            <v-layout fill-height
+                      justify-space-around align-center>
+                <v-flex md4 >
+                    <h1>有序</h1>
+                    <h2>开源任务管理系统</h2>
+                    <h3><span style="font-weight: bold">有序</span>是基于Vue-js以及Lumen框架开发的开源任务管理系统，包含敏捷模式模板，支持自定义模板。</h3>
                 </v-flex>
-                <v-flex sm12 md4>
+                <v-flex md4 >
                     <v-card align-center
                             justify-center>
                         <v-layout column slot="default"
                                   style="padding: 20px 40px;">
-                            <h1 style="text-align: center;padding: 10px">有序</h1>
+                            <h1 style="text-align: center;padding: 10px;">有序</h1>
                             <v-text-field
                                     label="手机号/邮箱"
                                     prepend-icon="assignment_ind"
@@ -32,6 +35,13 @@
                 </v-flex>
             </v-layout>
         </v-content>
+        <v-footer app >
+            <v-layout align-center justify-center row>
+                <v-flex text-xs-center xs12>
+                    ©️Tommy Lee
+                </v-flex>
+            </v-layout>
+        </v-footer>
     </v-app>
 
 </template>
